c8032c1659b8e0fa9fe69023876b95763c225acc
Jec Castanos <jec.castanos@gmail.com>
2022-07-28 22:26:45 +0800

    script: image-to-npz

---
+++ image-to-npz.py
@@ -0,0 +1,55 @@
+import os
+import random
+
+import cv2
+import numpy as np
+
+DATADIR = "./images"
+
+
+def getCategories():
+    # image directories are categories
+    dirs = []
+
+    for dir in os.listdir(DATADIR):
+        if os.path.isdir(os.path.join(DATADIR, dir)):
+            dirs.append(dir)
+    return dirs
+
+
+def getTrainingData(image_size, categories):
+    data = []
+
+    for category in categories:
+        # path for data dir and categories
+        path = os.path.join(DATADIR, category)
+        label = categories.index(category)
+
+        for img in os.listdir(path):
+            # read image
+            file = os.path.join(path, img)
+            # convert to grayscale to reduce data size
+            grayImage = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
+            # resize image to have the same dimension
+            newImage = cv2.resize(grayImage, (image_size, image_size))
+            # add to array
+            data.append([newImage, label])
+    return data
+
+
+categories = getCategories()
+training_data = getTrainingData(200, categories)
+
+# shuffle to avoid bias
+random.shuffle(training_data)
+
+# convert to np array
+training_data = np.array(training_data, dtype="object")
+
+# split array
+X_train = training_data.T[0]
+y_train = training_data.T[1]
+
+# save to file
+np.savez("./veggies-dataset.npz", X_train=X_train)
+np.savez("./veggies-label.npz", y_train=y_train)
